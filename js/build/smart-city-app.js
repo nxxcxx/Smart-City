30be2d589d32b9525376387c4393c597decb71be
NIX <nxxcxx@live.com>
2014-11-09 17:29:42 +0700

    raycaster

--- js/build/smart-city-app.js
+++ js/build/smart-city-app.js
@@ -450,7 +450,7 @@
 		TWEEN.update(time);
 		animate(time);
 		
-		// intersectMouse(world.hub);
+		intersectMouse(world.hub.children[1]);
 
 		// renderer.render(scene, camera);
 		composer.render();
@@ -464,24 +464,27 @@
 
 		// --- test ray caster 3D object-mouse intersection todo: check platform's children if contain intersected mesh then do sth
 
-			// var raycaster = new THREE.Raycaster();
-			// function intersectMouse(mesh) {
+			var raycaster = new THREE.Raycaster();
+			function intersectMouse(mesh) {
 				
-			// 	var vector = new THREE.Vector3( mouse.x, mouse.y, 1 ).unproject( camera );
+				var mo = new THREE.Vector3( mouse.x, mouse.y, 1 ).unproject( camera );
 
-			// 	raycaster.set( camera.position, vector.sub( camera.position ).normalize() );
+				raycaster.set( camera.position, mo.sub( camera.position ).normalize() );
 
-			// 	var intersects = raycaster.intersectObject( mesh );
+				var intersects = raycaster.intersectObject( mesh , true ); // also check all descendants
 
-			// 	if ( intersects.length > 0 ) {
-			// 		var intersect = intersects[0];
-			// 		console.warn(intersects);
-			// 		if (intersect.object == mesh) {  // r69 now return face3
-			// 			console.warn(111);
-			// 		}
+				if ( intersects.length > 0 ) {
 
-			// 	}
-			// }
+					var intersect = intersects[0];
+
+					console.warn(intersects);
+
+					if (intersect.object == mesh) {  // r69 now return face3
+						console.warn(111);
+					}
+
+				}
+			}
 		
 
 
